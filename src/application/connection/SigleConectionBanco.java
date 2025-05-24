74838ddf98028604418f414100eeb94152520554
Rafael de Souza Alves <rafaellsouza.java@gmail.com>
2025-05-24 00:11:53 -0300

    iniciando conexao com o banco PostgreSql

---
+++ src/application/connection/SigleConectionBanco.java
@@ -0,0 +1,44 @@
+package application.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SigleConectionBanco {
+	
+	private static final String url = "jdbc:postgresql://localhost:5432/trabalho_estacio?autoReconect=true";
+	private static final String user = "postgres";
+	private static final String senha = "";
+	private static Connection connection = null;
+	
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	static {
+		conectar();
+	}
+	
+	public SigleConectionBanco() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+			
+			if (connection == null) {
+				Class.forName("org.postgresql.Driver"); 
+				connection = DriverManager.getConnection(url, user, senha);
+				connection.setAutoCommit(false); 
+				
+				
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module exercicio_estacio_loja {
     requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 
     opens application to javafx.graphics, javafx.fxml;
     opens application.controllers to javafx.fxml;
